e6f53d63c2a45e4aca51270feec13960037b40f6
Lucas GERARD <lucas.gerard@ynov.com>
2024-09-24 15:23:44 +0200

    Ajout exercice1 & exercice2 + commentaire

--- Exercice_1/exercice1.go
+++ Exercice_1/exercice1.go
@@ -1,22 +1,32 @@
 package exercice1
 
+import "sort"
+
 func Ft_coin(coins []int, amount int) int {
+	/***  VERFICATION DES SORTIES/ERREURS POSSIBLES  ***/
+
+	// Si amount est  égal à 0, il n'y a pas besoin de pièces
+	if amount == 0 {
+		return 0
+	}
+	// Si le tableau de pièces est vide, il est impossible d'atteindre le montant
+	if len(coins) == 0 {
+		return -1
+	}
+
+	// Trier le tableau de pièces de façon décroissante, pour éviter les problèmes provenant du tableau
+	sort.Sort(sort.Reverse(sort.IntSlice(coins)))
+
 	result := 0
-	long := len(coins)
-	rendu := make([]int, len(coins))
 
+	for _, coin := range coins {
+		for amount >= coin {
+			amount -= coin
+			result++
+		}
+	}
 	if amount != 0 {
-		for i := long - 1; i >= 0; i-- {
-			for amount >= coins[i] {
-				amount -= coins[i]
-				rendu[i]++
-				result = len(rendu)
-
-			}
-		}
-		if amount != 0 {
-			return -1
-		}
+		return -1
 	}
 	return result
 }
--- Exercice_2/exercice2.go
+++ Exercice_2/exercice2.go
@@ -1,4 +1,26 @@
 package exercice2
 
+import "sort"
+
 func Ft_missing(nums []int) int {
+	long := len(nums)
+	result := 0
+
+	// Tri du tableau nums
+	sort.Ints(nums)
+
+	// Vérification du nombre manquant
+	for i := 0; i < long; i++ {
+		if nums[i] != i {
+			result = i
+			break
+		}
+	}
+
+	// Si aucun nombre manquant n'a été trouvé, le resultat est assigné à long
+	if result == 0 && nums[long-1] == long-1 {
+		result = long
+	}
+
+	return result
 }
