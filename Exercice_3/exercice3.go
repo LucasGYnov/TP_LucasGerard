feb18a948feb77a9fa29b480cd8d220bfee286a6
Lucas GERARD <lucas.gerard@ynov.com>
2024-09-24 15:49:14 +0200

    Ajout commentaire du exercice3 & exercice4

--- Exercice_3/exercice3.go
+++ Exercice_3/exercice3.go
@@ -3,14 +3,19 @@ package Exercice_3
 import "sort"
 
 func Ft_non_overlap(intervals [][]int) int {
+	// Si le tableau d'intervalles est vide, il n'y a rien à retirer.
 	if len(intervals) == 0 {
 		return 0
 	}
+
+	// Trier les intervalles
 	sort.Slice(intervals, func(i, j int) bool {
 		return intervals[i][1] < intervals[j][1]
 	})
-	count := 0
-	end := intervals[0][1]
+
+	count := 0             // Initalisation d'un compteur pour le nombre d'intervalles à retirer
+	end := intervals[0][1] // Initialiser 'end' avec la fin du premier intervalle
+
 	for i := 1; i < len(intervals); i++ {
 		if intervals[i][0] < end {
 			count++
@@ -18,5 +23,6 @@ func Ft_non_overlap(intervals [][]int) int {
 			end = intervals[i][1]
 		}
 	}
+
 	return count
 }
--- Exercice_4/exercice4.go
+++ Exercice_4/exercice4.go
@@ -1,13 +1,15 @@
 package Exercice_4
 
 func Ft_profit(prices []int) int {
+	// Si le tableau de prix est vide, il n'y a aucun bénéfice
 	if len(prices) == 0 {
 		return 0
 	}
 
-	minPrice := prices[0]
+	minPrice := prices[0] // Initialiser le prix minimum
 	maxProfit := 0
 
+	// Parcourir les prix à partir du deuxième élément
 	for _, price := range prices[1:] {
 		currentProfit := price - minPrice
 
